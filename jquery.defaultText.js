6d33b7aac49991fb0c33605b78b814dadebaab39
Marian <nemo@ikkoku.de>
2012-07-04 19:38:39 +0200

    Remove trailing whitespaces

--- jquery.defaultText.js
+++ jquery.defaultText.js
@@ -20,24 +20,24 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ;(function($) {
 	$.fn.defaultText = function(settings) {
 		var config = {text: 'Default Text', dataField: 'data-desc', clearOnSubmit: true};
-		
+
 		if(typeof settings == "string") {
 			config.text = settings;
 		}
 		else if(settings instanceof Object) {
 			$.extend(config, settings);
 		}
-		
+
 		this.filter('input, textarea').each(function() {
 			var obj = $(this);
-			
+
 			if(obj.val().length == 0) {
 				obj.val(config.text);
 			}
-			
+
 			obj.attr(config.dataField, config.text);
 			obj.addClass('jquery-inputdesc');
-			
+
 			obj.focus(onfocus);
 			obj.blur(onblur);
 			
@@ -45,27 +45,27 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 				obj.closest('form').submit(function() { onsubmit(obj); });
 			}
 		});
-		
+
 		function onfocus(e) {
 			var obj = $(e.currentTarget);
 			if(obj.val() == obj.attr(config.dataField)) {
 				obj.val('');
 			}
 		}
-		
+
 		function onblur(e) {
 			var obj = $(e.currentTarget);
 			if(obj.val().length == 0) {
 				obj.val(obj.attr(config.dataField));
 			}
 		}
-		
+
 		function onsubmit(field) {
 			if(field.val() == field.attr(config.dataField)) {
 				field.val('');
 			}
 		}
-		
+
 		return this;
 	};
 })(jQuery);
